3bc02ef374b00e4c035db020b0dd68708b1998cc
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-06 22:59:37 +0200

    Add one more command (bell add schedule). Add files for custom types for the new commands.

--- src/bell/commands/add/__init__.py
+++ src/bell/commands/add/__init__.py
@@ -1,8 +1,9 @@
 import typer
 
-from . import grade, student
+from . import grade, schedule, student
 
 app = typer.Typer()
 
 app.add_typer(grade.app)
 app.add_typer(student.app)
+app.add_typer(schedule.app)
