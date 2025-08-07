9646115388e4a0d155e82ae1220d49c14a9df5d2
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-07 22:47:04 +0200

    Add cmd-validation for bell add grade.

--- src/bell/commands/add/grade.py
+++ src/bell/commands/add/grade.py
@@ -11,16 +11,24 @@ app = typer.Typer()
 
 @app.command()
 def grade(
-    student: Annotated[Student, typer.Argument(parser=Student.parse, help="")],
-    grade: Annotated[Grade, typer.Argument(parser=Grade.parse, help="")],
+    student: Annotated[Student, typer.Argument(parser=Student.parse, help="")] = None,
+    grade: Annotated[Grade, typer.Argument(parser=Grade.parse, help="")] = None,
     date: Annotated[
         Date,
-        typer.Option(
-            datetime.now().strftime("%d-%m"), "--date", "-d", parser=Date.parse, help=""
-        ),
-    ],
+        typer.Option("--date", "-d", parser=Date.parse, help=""),
+    ] = datetime.now().strftime("%d-%m"),
     oral: bool = typer.Option(False, "--oral", "-o", help=""),
     comment: str = typer.Option("", "--comment", "-c", help=""),
     exam: bool = typer.Option(False, "--exam", "-e", help=""),
 ):
+    if exam and (student or grade or oral or comment != ""):
+        raise typer.BadParameter(
+            "If --exam flag is set, no other arguments and options (besides --date) must be provided."
+        )
+
+    if not student or not grade:
+        raise typer.BadParameter(
+            "If --exam is not set, both student and grade must be provided."
+        )
+
     run(student, grade, date, oral, comment, exam)
--- src/bell/core/add/grade.py
+++ src/bell/core/add/grade.py
@@ -4,4 +4,4 @@ from bell.types.cmd_args.add import Date, Grade, Student
 def run(
     student: Student, grade: Grade, date: Date, oral: bool, comment: str, exam: bool
 ):
-    pass
+    print(f"studen={student}, grade={grade}, date={date}, {oral=}, {comment=}, {exam=}")
