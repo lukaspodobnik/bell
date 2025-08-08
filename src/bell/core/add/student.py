2f3048ed3370fa3f1ebd29d41bb16ba2f850b0a5
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-08 14:05:44 +0200

    Add core logic for bell add student (single student).

--- src/bell/core/add/student.py
+++ src/bell/core/add/student.py
@@ -1,8 +1,35 @@
+from pathlib import Path
+
+import pandas as pd
+
 from bell.types.cmd_args.add import Student
 
 
 def run(student: Student, multiple: bool):
     print(f"student: {student}, multiple: {multiple}")
 
+    students_path = Path("students.csv")
+    df = pd.read_csv(students_path)
 
-# TODO: start here next time.
+    if student:
+        df = add_single_student(df, student)
+    else:
+        df = add_multiple_students(df)
+
+    df.to_csv(students_path, index=False)
+
+
+def add_single_student(df: pd.DataFrame, student: Student) -> pd.DataFrame:
+    next_id = df["id"].max() + 1 if not df.empty else 1
+
+    new_row = {
+        "id": next_id,
+        "first_name": student.first_name,
+        "last_name": student.last_name,
+    }
+
+    return pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+
+
+def add_multiple_students(df: pd.DataFrame) -> pd.DataFrame:
+    pass
