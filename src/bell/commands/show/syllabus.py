c844c4ccc88848efbbea68271b44925c732d1e0c
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-08 23:26:11 +0200

    Add bell show syllabus logic and one singular syllabus for testing.

--- src/bell/commands/show/syllabus.py
+++ src/bell/commands/show/syllabus.py
@@ -1,8 +1,10 @@
 import typer
 
+from bell.core.show.syllabus import run
+
 app = typer.Typer()
 
 
 @app.command()
 def syllabus():
-    pass
+    run()
---
+++ src/bell/core/show/syllabus.py
@@ -0,0 +1,31 @@
+from importlib.resources import files
+from pathlib import Path
+
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.panel import Panel
+
+from bell.res.syllabi import computer_science, maths
+from bell.types.enums.subjects import Subject
+
+
+# TODO: pick appropriate color for background
+def run():
+    subject, level = _get_subject_and_level()
+    match subject:
+        case Subject.MATHS:
+            src = files(maths)
+        case Subject.COMPUTER_SCIENCE:
+            src = files(computer_science)
+
+    src /= f"{level:02}.md"
+    md = Markdown(src.read_text())
+    panel = Panel(md, style="on color(4)")
+    Console().print(panel)
+
+
+def _get_subject_and_level():
+    class_path = Path(".").resolve()
+    level = class_path.name[:-1]
+    subject = Subject(class_path.parent.name)
+    return subject, int(level)
