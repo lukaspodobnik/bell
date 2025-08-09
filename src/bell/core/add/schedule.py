44ba381aeb71da2b52591532d4b9dc2f2e6f9b91
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-09 21:59:28 +0200

    Add core logic to bell add schedule.

--- src/bell/core/add/schedule.py
+++ src/bell/core/add/schedule.py
@@ -1,6 +1,19 @@
+from pathlib import Path
+
+import pandas as pd
+
 from bell.types.cmd_args.add import Slot
+from bell.types.enums.subjects import Subject
 from bell.types.enums.weekdays import Weekday
 
 
 def run(slot: Slot, weekday: Weekday):
-    print(f"slot={slot}, weekday={weekday.name}")
+    schedule_path = Path("..") / Path("..") / Path("schedule.csv")
+    class_path = Path(".").resolve()
+    df = pd.read_csv(schedule_path, index_col="Slot")
+
+    df.at[slot.num, weekday.value.capitalize()] = (
+        f"{class_path.name}-{Subject(class_path.parent.name).name}"
+    )
+
+    df.to_csv(schedule_path)
