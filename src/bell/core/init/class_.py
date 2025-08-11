6b45bbe934ccd5c73ecf49fef6f4484b4087403d
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-11 16:27:34 +0200

    Modify init class and init year, so it write to the hidden structure validation file.

--- src/bell/core/init/class_.py
+++ src/bell/core/init/class_.py
@@ -1,52 +1,50 @@
 from importlib.resources import files
 from pathlib import Path
 
+import typer
 import yaml
 
 from bell.res.templates import class_ as class_templates
 from bell.types.cmd_args.init import Class_
 from bell.types.enums.subjects import Subject
-from bell.utils.clone_files import clone_csv
+from bell.utils.file_manager import clone_csv
 
 
 def run(class_: Class_, subject: Subject):
-    class_path = _create_class_dir(class_, subject)
-    _create_notes_dir(class_path)
-    _create_students_csv(class_path)
-    _create_grades_csv(class_path)
+    class_path = Path(subject.value) / str(class_)
+    try:
+        create_class_dir(class_path)
+    except FileExistsError:
+        typer.echo("Class already initialized.")
+
+    create_notes_dir(class_path)
+    create_students_csv(class_path)
+    create_grades_csv(class_path)
+
+    crs_path = Path("..") / ".bell" / "classroom_structure" / Path.cwd().name / ".yaml"
+    crs = yaml.safe_load(crs_path.read_text())
+    crs[subject.value] = {
+        str(class_): {".notes": {}, ".students.csv": None, ".grades.csv": None},
+    }
+    crs_path.write_text(yaml.dump(crs))
 
 
-def _year_initialized() -> bool:
-    dotbell_path = Path("..") / Path(".bell.yaml")
-    if not dotbell_path.is_file():
-        print(".bell.yaml not found")
-        return False
-
-    dotbell = yaml.load(dotbell_path.read_text(), Loader=yaml.FullLoader)
-    if dotbell.get(f"{Path('.').resolve().name}_init", None) is None:
-        print("Year not initialized")
-        return False
-
-    return True
+def create_class_dir(class_path: Path) -> None:
+    class_path.parent.mkdir(exist_ok=True)
+    class_path.mkdir()
 
 
-def _create_class_dir(class_: Class_, subject: Subject) -> Path:
-    class_path = subject.value / Path(class_.name)
-    class_path.mkdir(parents=True, exist_ok=True)
-    return class_path
+def create_notes_dir(class_path: Path) -> None:
+    (class_path / ".notes").mkdir()
 
 
-def _create_notes_dir(class_path: Path) -> None:
-    (class_path / "notes").mkdir()
-
-
-def _create_students_csv(class_path: Path) -> None:
+def create_students_csv(class_path: Path) -> None:
     src = files(class_templates) / "students.csv"
-    dst = class_path / "students.csv"
+    dst = class_path / ".students.csv"
     clone_csv(src, dst)
 
 
-def _create_grades_csv(class_path: Path) -> None:
+def create_grades_csv(class_path: Path) -> None:
     src = files(class_templates) / "grades.csv"
-    dst = class_path / "grades.csv"
+    dst = class_path / ".grades.csv"
     clone_csv(src, dst)
--- src/bell/core/init/year.py
+++ src/bell/core/init/year.py
@@ -2,15 +2,18 @@ from importlib.resources import files
 from pathlib import Path
 
 import typer
+import yaml
 
 from bell.res.templates import year as year_templates
 from bell.types.cmd_args.init import Year
-from bell.utils.clone_files import clone_csv
+from bell.utils.file_manager import clone_csv
 
 
 def run(year: Year):
     if Path.cwd().name != "classroom":
-        typer.echo("Use this command from within your classroom (bell root directory).")
+        typer.echo(
+            "Use this command from within your classroom (your bell root directory)."
+        )
         return
 
     year_path = Path(str(year))
@@ -20,5 +23,9 @@ def run(year: Year):
         typer.echo("Year already initialized.")
         return
 
-    clone_csv(files(year_templates) / "schedule.csv", year_path / "schedule.csv")
-    (Path(".bell") / "classroom_structure" / str(year) / ".yaml").write_text("")
+    clone_csv(files(year_templates) / "schedule.csv", year_path / ".schedule.csv")
+
+    # create the year yaml at .bell/classroom_structure/
+    (Path(".bell") / "classroom_structure" / str(year) / ".yaml").write_text(
+        yaml.dump({".schedule.csv": None})
+    )
