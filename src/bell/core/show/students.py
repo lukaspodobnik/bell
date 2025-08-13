ffe57102f6a3877809f22beb16b36027a79c8ea3
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-13 23:08:23 +0200

    Add core logic for bell show students.

--- src/bell/core/show/students.py
+++ src/bell/core/show/students.py
@@ -1,2 +1,30 @@
+from pathlib import Path
+
+import pandas as pd
+import typer
+from rich.console import Console
+from rich.table import Table
+
+
 def run():
-    pass
+    students_path = Path(".students.csv")
+    if not students_path.is_file():
+        typer.echo("Use this command withing a classroom directory.")
+        return
+
+    df = pd.read_csv(students_path)
+    if df.empty:
+        typer.echo("No students found.")
+        return
+
+    table = Table()
+    df = df.sort_values(by=["last_name", "first_name"])
+    table.add_column("#", justify="right")
+    for col in df.columns:
+        table.add_column(str(col))
+
+    for i, row in enumerate(df.itertuples(index=False), start=1):
+        table.add_row(str(i), *[str(cell) for cell in row])
+        table.add_section()
+
+    Console().print(table)
