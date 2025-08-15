a897ee29177082e68ef61fae39301d5f9d46f559
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-15 14:19:21 +0200

    Just delete some unused stuff.

--- src/bell/utils/file_manager.py
+++ src/bell/utils/file_manager.py
@@ -1,16 +1,8 @@
 from pathlib import Path
 
 import pandas as pd
-import yaml
 
 
 def clone_csv(src: Path, dst: Path) -> None:
     df = pd.read_csv(src)
     df.to_csv(dst, index=False)
-
-
-def clone_yaml(src: Path, dst: Path) -> None:
-    src_content = src.read_text()
-    src_content = yaml.load(src_content, Loader=yaml.FullLoader)
-    dst_content = yaml.dump(src_content, sort_keys=False)
-    dst.write_text(dst_content)
